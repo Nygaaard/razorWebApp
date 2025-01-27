f9452ff50085a09f26b13d6cef54a0fac346326c
Nygaaard <nygardandreas@hotmail.com>
2025-01-27 10:39:45 +0100

    Added image slide function

--- wwwroot/js/main.js
+++ wwwroot/js/main.js
@@ -1,5 +1,18 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿
+//Eventlyssnare
+document.addEventListener("DOMContentLoaded", function() {
+    //Array med bilder
+    const images = ["./images/yellow-flowers.jpg", "./images/woman.jpg", "./images/sea.jpg"];
+    let currentIndex = 0;
 
-// Write your JavaScript code.
+    const imageEl = document.getElementById("image");
+    //Eventlyssnare vid klick på bild
+    imageEl.addEventListener("click", function() {
+        //Öka index med 1 och återställ till 0 vid arrayens slut
+        currentIndex = (currentIndex + 1) % images.length;
+        //Hämta ny bild
+        imageEl.src = images[currentIndex];
+    })
+})
+
 
